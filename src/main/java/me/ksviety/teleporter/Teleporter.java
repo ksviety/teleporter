3cacc722c23dab37a248f46b93a17a906e8cd834
ksviety <ksviety@gmail.com>
2021-07-31 01:22:08 +0500

    Initialized project

---
+++ src/main/java/me/ksviety/teleporter/Teleporter.java
@@ -0,0 +1,9 @@
+package me.ksviety.teleporter;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Teleporter.MOD_ID)
+public class Teleporter
+{
+    public static final String MOD_ID = "teleporter";
+}
