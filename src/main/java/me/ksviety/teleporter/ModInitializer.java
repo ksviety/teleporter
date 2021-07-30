30c1e0b0e736a650b3e6c263a49fec02ad4e4e7d
ksviety <ksviety@gmail.com>
2021-07-31 04:03:29 +0500

    Created ModInitializer

---
+++ src/main/java/me/ksviety/teleporter/ModInitializer.java
@@ -0,0 +1,16 @@
+package me.ksviety.teleporter;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ModInitializer.MOD_ID)
+public class ModInitializer
+{
+    public static final String MOD_ID = "teleporter";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+}
