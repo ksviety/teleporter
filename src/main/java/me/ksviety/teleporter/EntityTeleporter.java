e575f4a2baf8555d301150948c840931db4fe123
ksviety <ksviety@gmail.com>
2021-07-31 05:18:53 +0500

    Fix: EntityTeleporter

--- src/main/java/me/ksviety/teleporter/EntityTeleporter.java
+++ src/main/java/me/ksviety/teleporter/EntityTeleporter.java
@@ -1,6 +1,10 @@
 package me.ksviety.teleporter;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class EntityTeleporter {
     private final PositionProvider positionProvider;
@@ -12,10 +16,11 @@ public class EntityTeleporter {
     public void teleport(Entity entity) {
         Position position = positionProvider.provide();
 
-        entity.setPosition(
-                position.getX(),
-                position.getY(),
-                position.getZ()
+        final MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+
+        server.commandManager.executeCommand(
+                server,
+                String.format("/tp %s %d %d %d", entity.getName(), position.getX(), position.getY(), position.getZ())
         );
     }
 }
