d2afd87749bac7033fc2b8960e94b40c60f0d736
ksviety <ksveity@gmail.com>
2021-08-01 20:03:34 +0500

    Added bounds data to the config

--- src/main/java/me/ksviety/teleporter/Config.java
+++ src/main/java/me/ksviety/teleporter/Config.java
@@ -1,47 +1,57 @@
 package me.ksviety.teleporter;
 
 import java.io.*;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Config {
     @NotNull
     @Contract("_ -> new")
     public static Config fromFile(File file) throws FileNotFoundException {
         final BufferedReader reader = new BufferedReader(new FileReader(file));
-        final Set<String> bannedBlocks = new HashSet<>();
 
-        try {
-            String block;
+        final String json = reader.lines().collect(Collectors.joining());
+        final JSONObject config = new JSONObject(json);
 
-            while (true) {
-                block = reader.readLine();
+        final int centerX = config.getInt("centerX");
+        final int centerZ = config.getInt("centerZ");
+        final int size = config.getInt("size");
+        final String[] bannedBlocksArray = (String[]) config.getJSONArray("bannedBlocks").toList().toArray();
 
-                if (block == null)
-                    break;
+        final Set<String> bannedBlocks = new HashSet<>(Arrays.asList(bannedBlocksArray));
 
-                bannedBlocks.add(block);
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new InternalError("Cannot read config file!");
-        }
-
-        return new Config(bannedBlocks);
+        return new Config(bannedBlocks, centerX, centerZ, size);
     }
 
     private final Set<String> bannedBlocks;
+    private final int centerX;
+    private final int centerZ;
+    private final int size;
 
-    private Config(Set<String> bannedBlocks) {
+    private Config(Set<String> bannedBlocks, int centerX, int centerZ, int size) {
         this.bannedBlocks = bannedBlocks;
+        this.centerX = centerX;
+        this.centerZ = centerZ;
+        this.size = size;
     }
 
     public Collection<String> getBannedBlocks() {
         return bannedBlocks;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCenterZ() {
+        return centerZ;
+    }
+
+    public int getCenterX() {
+        return centerX;
     }
 }
