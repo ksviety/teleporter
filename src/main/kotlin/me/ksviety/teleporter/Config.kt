fc7fe1f3a093f75f2f7fa8394f777220bf750378
ksviety <ksveity@gmail.com>
2021-08-11 22:31:39 +0500

    Created default config

--- src/main/kotlin/me/ksviety/teleporter/Config.kt
+++ src/main/kotlin/me/ksviety/teleporter/Config.kt
@@ -1,13 +1,6 @@
 package me.ksviety.teleporter
 
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
-import java.util.HashSet
-import java.util.stream.Collectors
-
-class Config private constructor(
+class Config(
     private val bannedBlocks: Set<String>,
     val centerX: Int,
     val centerZ: Int,
@@ -20,25 +13,10 @@ class Config private constructor(
     }
 
     companion object {
-
-        fun fromFile(file: File): Config {
-            val reader = BufferedReader(FileReader(file))
-            val json = reader.lines().collect(Collectors.joining())
-            val config = JSONObject(json)
-
-            val centerX = config.getInt("centerX")
-            val centerZ = config.getInt("centerZ")
-            val size = config.getInt("size")
-            val bannedBlocksArray = config.getJSONArray("bannedBlocks")
-            val shiftRadius = config.getInt("shiftRadius")
-            val searchIterationsLimit = config.getInt("searchIterationsLimit")
-            val bannedBlocks: MutableSet<String> = HashSet()
-
-            for (i in bannedBlocksArray.toList().indices) {
-                bannedBlocks.add(bannedBlocksArray.getString(i))
-            }
-
-            return Config(bannedBlocks, centerX, centerZ, size, shiftRadius, searchIterationsLimit)
-        }
+        val default = Config(
+            setOf("lava", "water"),
+            0, 0, 1000,
+            200, 10_000
+        )
     }
 }
