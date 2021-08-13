2bc096fed540d199ac5ab37e764f557290a2e099
ksviety <ksveity@gmail.com>
2021-08-13 22:23:10 +0500

    Delegated config loading to corresponding object

--- src/main/kotlin/me/ksviety/teleporter/Teleporter.kt
+++ src/main/kotlin/me/ksviety/teleporter/Teleporter.kt
@@ -11,15 +11,13 @@ import me.ksviety.teleporter.teleporters.CommandEntityTeleporter
 import me.ksviety.teleporter.providers.SafePositionProvider
 import me.ksviety.teleporter.providers.BoundRandomPositionProvider
 import me.ksviety.teleporter.exceptions.CannotFindClosetSafePositionException
+import me.ksviety.teleporter.loaders.ConfigFileLoader
 import me.ksviety.teleporter.loaders.TeleportationCacheFileLoader
 import me.ksviety.teleporter.savers.TeleportationCacheFileSaver
 import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.util.text.TextComponentString
-import java.io.IOException
 import net.minecraft.entity.Entity
 import net.minecraftforge.fml.common.Mod
-import java.io.File
-import java.io.FileNotFoundException
 import java.net.URI
 import java.security.SecureRandom
 
@@ -29,13 +27,14 @@ class Teleporter {
     private lateinit var config: Config
 
     private val teleportationCacheFileLocation = "./teleportation.cache"
+    private val configFileLocation = "./config/teleportation.config"
 
     @Mod.EventHandler
     fun preInit(event: FMLPreInitializationEvent) {
         MinecraftForge.EVENT_BUS.register(this)
 
         teleportationCache = TeleportationCacheFileLoader(URI(teleportationCacheFileLocation)).load()
-        config = loadConfig()
+        config = ConfigFileLoader(URI(configFileLocation)).load()
     }
 
     @Mod.EventHandler
@@ -70,26 +69,6 @@ class Teleporter {
             (player as EntityPlayerMP).connection.disconnect(
                 TextComponentString("Could not find any safe position to spawn, log in again.")
             )
-        }
-    }
-
-    private fun loadConfig(): Config {
-        val configFile = File("./config/teleporter.config")
-
-        if (!configFile.exists()) {
-            try {
-                configFile.createNewFile()
-            } catch (e: IOException) {
-                e.printStackTrace()
-                throw InternalError("Could not create cache file!")
-            }
-        }
-
-        return try {
-            Config.fromFile(configFile)
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-            throw InternalError("Could not create cache file!")
         }
     }
 
