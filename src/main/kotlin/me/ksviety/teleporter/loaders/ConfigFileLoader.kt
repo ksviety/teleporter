230b7474d3d729a6c38598e8a5e0e1b37cb3bdfe
ksviety <ksveity@gmail.com>
2021-08-14 00:59:42 +0500

    Migrated from org.json to gson

--- src/main/kotlin/me/ksviety/teleporter/loaders/ConfigFileLoader.kt
+++ src/main/kotlin/me/ksviety/teleporter/loaders/ConfigFileLoader.kt
@@ -1,26 +1,20 @@
 package me.ksviety.teleporter.loaders
 
+import com.google.gson.JsonParser
 import me.ksviety.teleporter.Config
-import org.json.JSONObject
 import java.net.URI
-import java.util.HashSet
 
 class ConfigFileLoader(path: URI) : FileLoader<Config>(path) {
 
     override fun loadFromContent(content: String): Config {
-        val json = JSONObject(content)
+        val json = JsonParser().parse(content).asJsonObject
 
-        val centerX = json.getInt("centerX")
-        val centerZ = json.getInt("centerZ")
-        val size = json.getInt("size")
-        val bannedBlocksArray = json.getJSONArray("bannedBlocks")
-        val shiftRadius = json.getInt("shiftRadius")
-        val searchIterationsLimit = json.getInt("searchIterationsLimit")
-        val bannedBlocks: MutableSet<String> = HashSet()
-
-        for (i in bannedBlocksArray.toList().indices) {
-            bannedBlocks.add(bannedBlocksArray.getString(i))
-        }
+        val centerX = json.get("centerX").asInt
+        val centerZ = json.get("centerZ").asInt
+        val size = json.get("size").asInt
+        val bannedBlocks = json.get("bannedBlocks").asJsonArray.map { it.asString }.toSet()
+        val shiftRadius = json.get("shiftRadius").asInt
+        val searchIterationsLimit = json.get("searchIterationsLimit").asInt
 
         return Config(bannedBlocks, centerX, centerZ, size, shiftRadius, searchIterationsLimit)
     }
