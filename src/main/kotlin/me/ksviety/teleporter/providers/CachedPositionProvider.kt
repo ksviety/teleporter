09ff7aeda5b7e10920081a358600ac249f87bfea
ksviety <ksveity@gmail.com>
2021-08-15 03:59:12 +0500

    Created CachedPositionProvider

---
+++ src/main/kotlin/me/ksviety/teleporter/providers/CachedPositionProvider.kt
@@ -0,0 +1,14 @@
+package me.ksviety.teleporter.providers
+
+import me.ksviety.teleporter.PositionProvider
+
+/**
+ * Saves the position it provides first time,
+ * so the following calls will simply return
+ * the cached value instead of recalculating
+ */
+class CachedPositionProvider(private val original: PositionProvider) : PositionProvider {
+    private val cache by lazy(original::provide)
+
+    override fun provide() = cache
+}
