cd22b728a755d7b8ebfd750c6cf9cb9fa13f8472
ksviety <43730934+ksviety@users.noreply.github.com>
2021-08-19 05:07:53 +0500

    Created AsyncTeleporter

---
+++ src/main/kotlin/me/ksviety/teleporter/teleporters/AsyncTeleporter.kt
@@ -0,0 +1,24 @@
+package me.ksviety.teleporter.teleporters
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import me.ksviety.teleporter.Teleporter
+import me.ksviety.teleporter.exceptions.TeleportationException
+
+abstract class AsyncTeleporter<in T>(
+    private val scope: CoroutineScope,
+    private val teleporter: Teleporter<T>,
+    ) : Teleporter<T> {
+
+    protected abstract fun onTeleportationFailed(cause: TeleportationException)
+
+    final override fun teleport(obj: T) {
+        scope.launch {
+            try {
+                teleporter.teleport(obj)
+            } catch (exception: TeleportationException) {
+                onTeleportationFailed(exception)
+            }
+        }
+    }
+}
