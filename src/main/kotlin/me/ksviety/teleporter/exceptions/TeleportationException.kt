b22c2859fc7b52dd740658d3e3528f7b5b809838
ksviety <43730934+ksviety@users.noreply.github.com>
2021-08-19 05:06:22 +0500

    Created TeleportationException

---
+++ src/main/kotlin/me/ksviety/teleporter/exceptions/TeleportationException.kt
@@ -0,0 +1,5 @@
+package me.ksviety.teleporter.exceptions
+
+import me.ksviety.teleporter.exceptions.TeleporterException
+
+class TeleportationException(message: String?, cause: Throwable?) : TeleporterException(message, cause)
