96b6d6724d256933e1def874c0f14986a84ba82a
ksviety <43730934+ksviety@users.noreply.github.com>
2021-08-19 07:31:33 +0500

    Extracted disconnect message to constructor

--- src/main/kotlin/me/ksviety/teleporter/teleporters/failHandlers/DisconnectingTeleportationFailHandler.kt
+++ src/main/kotlin/me/ksviety/teleporter/teleporters/failHandlers/DisconnectingTeleportationFailHandler.kt
@@ -6,13 +6,11 @@ import me.ksviety.teleporter.utilities.PlayerDisconnector
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.text.TextComponentString
 
-class DisconnectingTeleportationFailHandler : TeleportationFailHandler<EntityPlayer> {
+class DisconnectingTeleportationFailHandler(private val message: String) : TeleportationFailHandler<EntityPlayer> {
 
     override fun handle(target: EntityPlayer, cause: TeleportationException) {
         PlayerDisconnector(target).disconnect(
-            TextComponentString(
-                "Could not find any good point to teleport to"
-            )
+            TextComponentString(message)
         )
     }
 }
