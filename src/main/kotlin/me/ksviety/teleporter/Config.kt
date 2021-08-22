a1c35cf561ad2a143dc399570a049e9f87419dcf
ksviety <43730934+ksviety@users.noreply.github.com>
2021-08-22 18:14:47 +0500

    Converted Config to interface

--- src/main/kotlin/me/ksviety/teleporter/Config.kt
+++ src/main/kotlin/me/ksviety/teleporter/Config.kt
@@ -2,25 +2,19 @@ package me.ksviety.teleporter
 
 import net.minecraft.util.math.Vec3i
 
-class Config(
-    private val bannedBlocks: Set<String>,
-    val centerX: Int,
-    val centerZ: Int,
-    val size: Int,
-    val shiftRadius: Int,
-    val searchIterationsLimit: Int,
-    val spawn: Vec3i
-) {
-    fun getBannedBlocks(): Collection<String> {
-        return bannedBlocks
-    }
+interface Config {
 
-    companion object {
-        val default = Config(
-            setOf("lava", "water"),
-            0, 0, 1000,
-            200, 10_000,
-            Vec3i(0, 100, 0)
-        )
-    }
+    fun readBannedBlocks(): Collection<String>
+
+    fun readCenterX(): Int
+
+    fun readCenterZ(): Int
+
+    fun readSize(): Int
+
+    fun readShiftRadius(): Int
+
+    fun readSearchIterationsLimit(): Int
+
+    fun readSpawnPosition(): Vec3i
 }
